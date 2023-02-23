42da4b6c062b367bc022325b1fe4b459cb861b32
Stephen Olabode <d.bodestephen@gmail.com>
2023-02-23 13:33:29 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *prototype: int mul(int a, int b) function that multiplies two integers
+ *
+ * @c: input
+ *
+ * Return: 1 if is digit, 0 if not
+*/
+
+int mul(int a, int b){
+	return a * b;
+}
