7598715b46fa881a5536ebd9b79e074da213db9f
Stephen Olabode <d.bodestephen@gmail.com>
2023-02-27 07:03:58 -0800

    copies a string from one pointer to another including the terminating null byte

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,8 @@
  * _strcpy - copies a string from one pointer to another
  *	including the terminating null byte (\0)
  *
- * @src: source of string parameter input
- * @dest: destination of string
+ * @src: The source of string parameter input
+ * @dest: the destination of string
  *
  * Return: pointer to dest input parameter
 */
