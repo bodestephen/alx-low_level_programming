0c11afab4c31e96611ca4b15c4d162d02257a6f9
Stephen Olabode <d.bodestephen@gmail.com>
2023-02-27 07:37:23 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,28 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
 
-int main(void) {
-    srand(time(NULL)); /* Seed the random number generator with the current time */
-    
-    int i; /* Declare i before password to avoid mixed declarations and code */
-    char password[PASSWORD_LENGTH + 1]; /* Allocate space for the password string */
+/**
+ *
+ *
+ */
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        int rand_num = rand() % 62; /* Generate a random number between 0 and 61 */
-        if (rand_num < 26) { /* If rand_num is between 0 and 25, add a random lowercase letter */
-            password[i] = 'a' + rand_num;
-        } else if (rand_num < 52) { /* If rand_num is between 26 and 51, add a random uppercase letter */
-            password[i] = 'A' + rand_num - 26;
-        } else { /* If rand_num is between 52 and 61, add a random digit */
-            password[i] = '0' + rand_num - 52;
-        }
-    }
-    password[PASSWORD_LENGTH] = '\0'; /* Add a null terminator to the end of the password string */
-    
-    printf("%s\n", password); /* Print the generated password */
-    
-    return 0;
+int main(void)
+{
+	srand((unsigned int)(time(NULL)));
+
+	int index = 0;
+	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+	for(index = 0; index < 12; index++)
+	{
+		printf("%c", characters[rand() % (sizeof characters - 1)]);
+	}
 }
-
