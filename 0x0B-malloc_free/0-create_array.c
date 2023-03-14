c4d13dedee87f0ab19327ed8d73c1d466723bb67
Stephen Olabode <d.bodestephen@gmail.com>
2023-03-14 08:01:29 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str) {
+    if (str == NULL) {
+        return NULL;
+    }
+
+    char *dup_str = malloc(strlen(str) + 1);
+
+    if (dup_str == NULL) {
+        return NULL;
+    }
+
+    strcpy(dup_str, str);
+
+    return dup_str;
+}
+
