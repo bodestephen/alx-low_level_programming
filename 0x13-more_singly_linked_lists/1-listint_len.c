682ee5cec570da399bf87bc5f4c0513331ed11dd
Stephen Olabode <d.bodestephen@gmail.com>
2023-04-05 13:26:12 -0700

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,20 @@
 #include "lists.h"
+
 /**
- * Counts the number of elements in the given list
- * @param h A pointer to the start of the list.
- * @return The number of elements in the list.
+ * Return the number of elements in a list
+ *
+ * @param h the head of the list
+ * @return the number of elements in the list
  */
-size_t listint_len(const listint_t *h) {
+size_t listint_len(const listint_t *h)
+{
     size_t count = 0;
-    while (h) {
+
+    while (h != NULL) {
         count++;
         h = h->next;
     }
+
     return count;
 }
 
