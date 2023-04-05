29ad62e089b873e510f8dea19aa59ad0229d4866
Stephen Olabode <d.bodestephen@gmail.com>
2023-04-05 14:07:40 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,35 +1,41 @@
-#include <stdlib.h>
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - delete a node at an index
+ * @head: start point
+ * @index: specified position
+ * Return: 1 if successful, -1 if failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev;
-    unsigned int i;
+	listint_t *prev;
+	listint_t *current = *head;
+	unsigned int a;
 
-    if (*head == NULL)
-        return (-1);
+	if (!head || !(*head))
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
 
-    prev = *head;
-    current = (*head)->next;
-
-    for (i = 1; current != NULL && i < index; i++)
-    {
-        prev = current;
-        current = current->next;
-    }
-
-    if (current == NULL)
-        return (-1);
-
-    prev->next = current->next;
-    free(current);
-    return (1);
+	for (a = 0; current && (a <= index); a++)
+	{
+		if (a == index)
+		{
+			prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		else
+		{
+			prev = current;
+			current = current->next;
+		}
+	}
+	return (-1);
 }
-
+/* Signature: By Bode Stephen */ 
