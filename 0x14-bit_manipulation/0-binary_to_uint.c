5ac19ad892df3477a6408d904ae3c42b81f389e2
Stephen Olabode <d.bodestephen@gmail.com>
2023-04-10 09:16:48 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,6 +5,7 @@
  * @b: points to a string of 0's and 1's
  * Return: decimal format of the binary
  */
+/* Code by bode stephen*/
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0; /* dec is the variable to hold the decimal format */
